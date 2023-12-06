4fd54ee9923a0506f72ef29b4140175e353a5f91
SSMSI - Service statistique ministériel de la sécurité intérieure <104771345+ssmsi@users.noreply.github.com>
2023-12-06 16:01:32 +0100

    Update lancementApplication.R

--- lancementApplication.R
+++ lancementApplication.R
@@ -1,22 +1,5 @@
 library("shiny")
-repertoire = "M:/Commun/Territoires/Diffusion/Application/AppTerritoires/donneesTerritoriales/"
-setwd(repertoire)
 
 # # Lancer l'application dans la console R
 # runApp(repertoire)
-
-# # Déploiement de l'appli
-library('rsconnect')
-
-# rsconnect::setAccountInfo(name='ssmsi',
-#                           token='CA22DD1C2191C1D6C86DFE839359D50E',
-#                           secret='GmhKZECrVS2H6QHCmEs6j8zdt2xhkOI4rDCYXjCR')
-#                           # token = 'BFDBFF87C5889195CCCEA39450D7140F',
-#                           # secret = 'o6pjmlaMDn9PVDpUxN17rEoSgA86ocweQZKZYNDq')
-
-options(rsconnect.locale = 'your_locale')
-rsconnect::appDependencies()
-rsconnect::deployApp(repertoire,account='ssmsi')
-
-# rsconnect::showLogs(repertoire,account='ssmsi')
 
